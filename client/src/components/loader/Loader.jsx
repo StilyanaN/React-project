bddf0d92a6d671730f5ee201195bbad7121ada80
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-07-22 01:57:44 +0300

    Add loader component

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,19 @@
+import "./Loader.css"
+
+export default function Loader() {
+  return (
+    <div className="page-wrapper">
+        
+      <div className="loader">
+        <div className="jelly">
+          <div className="body"></div>
+          <div className="stick"></div>
+          <div className="eye"></div>
+          <div className="eye"></div>
+          <div className="mouth"></div>
+        </div>
+        <div className="shadow"></div>
+      </div>
+    </div>
+  );
+}
