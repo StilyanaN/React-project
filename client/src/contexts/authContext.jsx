ffa78ce3103a1e289da493900c866b3c01c35948
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-07-27 02:14:23 +0300

    Add auth context

---
+++ client/src/contexts/authContext.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import * as authService from '../services/authService';
+import usePersistedState from "../hooks/usePersistedState";
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+
+    const isAdmin = Boolean(auth.role === "admin");
+
+    const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+
+        setAuth(result);
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate("/");
+    };
+
+    const registerSubmitHandler = async (values) => {
+        const result = await authService.register(values.username, values.email, values.password);
+
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate("/");
+    };
+
+    const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
+    };
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        isAdmin,
+        username: auth.username || auth.email,
+        email: auth.email,
+        userId: auth._id,
+        isAuthenticated: !!auth.accessToken,
+    };
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+
+export default AuthContext;
