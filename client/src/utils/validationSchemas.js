8641a5623e725cd142dc744341f0e86a9017835f
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-07-30 02:14:30 +0300

    Change schema  bulder to yup

--- client/src/utils/validationSchemas.js
+++ client/src/utils/validationSchemas.js
@@ -1,19 +1,15 @@
-// src/utils/validationSchemas.js
-import * as z from "zod";
+import * as yup from 'yup';
 
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(abv\.bg|gmail\.com)$/i;
 
-export const loginSchema = z.object({
-  email: z.string().trim().min(1, 'Email is required.').email("Invalid email address.").regex(emailRegex, "Invalid email address."),
-  password: z.string().trim().min(1, 'Password is required.').min(3, 'Password must be at least 6 characters long.')
+export const loginSchema = yup.object().shape({
+  email: yup.string().trim().min(1, 'Email is required.').email("Invalid email address.").matches(emailRegex, "Invalid email address."),
+  password: yup.string().trim().min(1, 'Password is required.').min(3, 'Password must be at least 6 characters long.')
 });
 
-export const registerSchema = z.object({
-    name: z.string().trim().min(1, 'Name is required.').min(2, 'Name must be at least 2 characters long.'),
-    email: z.string().trim().min(1, 'Email is required.').email("Invalid email address.").regex(emailRegex, "Invalid email address."),
-    password: z.string().trim().min(6, 'Password must be at least 6 characters long.'),
-    repassword: z.string().trim().min(6, 'Password confirmation is required.')
-  }).refine((data) => data.password === data.repassword, {
-    path: ['repassword'],
-    message: "Passwords don't match.",
-  });
+export const registerSchema = yup.object({
+    name: yup.string().trim().min(1, 'Name is required.').min(2, 'Name must be at least 2 characters long.'),
+    email: yup.string().trim().min(1, 'Email is required.').email("Invalid email address.").matches(emailRegex, "Invalid email address."),
+    password: yup.string().trim().min(6, 'Password must be at least 6 characters long.'),
+    repassword: yup.string().trim().min(6, 'Password confirmation is required.').oneOf([yup.ref('password'), null],"Passwords don't match.")
+})
