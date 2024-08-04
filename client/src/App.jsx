7348f00d7c3514183a38f9730ea9ad2051483480
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-08-04 22:48:23 +0300

    Add more routes and gusrds

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from "./contexts/authContext";
+import { CartProvider } from "./contexts/cartContext";
 import Topbar from "./components/top-bar/Topbar";
 import Navigation from "./components/navigation/Navigation";
 import Home from "./components/home/Home";
@@ -11,15 +14,22 @@ import ProductDetails from "./components/products/product-details/ProductDetails
 import About from "./components/home/about/About";
 import Register from "./components/register/Register";
 import SideCatalog from "./components/products/side-catalog/SideCatalog";
+import Logout from "./components/logout/Logout";
 import Loader from "./components/loader/Loader";
-
+import Cart from "./components/shopping-cart/Cart";
+import Checkout from "./components/shopping-cart/checkout/Checkout";
+import CreteProduct from "./components/admin/create-product/CreateProduct";
+import EditProduct from "./components/admin/edit-product/EditProduct";
+import AuthGuard from "./components/guards/AuthGuard";
+import AdminGuard from "./components/guards/AdminGuard"
+import OrderHistory from "./components/orders/Order";
 
 
 function App() {
   return (
     <>
       <AuthProvider>
-       
+        <CartProvider>
           <Topbar />
           <Navigation />
           <Routes>
@@ -31,10 +41,22 @@ function App() {
             <Route path={"/register"} element={<Register />} />
             <Route path={"/catalog"} element={<Catalog />} />
             <Route path={"/catalog/:flavorId"} element={<ProductDetails />} />
+            <Route element={<AuthGuard />}>
+            <Route path={"/cart"} element={<Cart />} />
+            <Route path={"/checkout"} element={<Checkout />} />
+            <Route path={"/profile/orders"} element={<OrderHistory />} />
+            <Route path={"/logout"} element={<Logout />} />
+
+            </Route>
+            <Route element={<AdminGuard/>}> 
+            <Route path={"/create"} element={<CreteProduct />} />
+            <Route path={"/catalog/:flavorId/edit"} element={<EditProduct />} />
+            </Route>
           </Routes>
 
           <Footer />
-        
+          <ToastContainer />
+        </CartProvider>
       </AuthProvider>
     </>
   );
