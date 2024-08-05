477696f2140acd9d80ef1a001432dbc10b331cc1
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-08-05 18:26:19 +0300

    Set video modal

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,22 +1,39 @@
-import Carousel from "./carousel/Carousel"
-import About from "./about/About"
-import Promotion from "./promotion/Promotion"
-import VideoModal from "./video-modal/VideoModal"
-import Portfolio from "./portfolio/Portfolio"
-import Products from "./products-carousel/ProductsCarousel"
-import Team from "./team/Team"
 
-export default function Home(){
+import  { useState } from 'react';
+import Carousel from './carousel/Carousel';
+import About from './about/About';
+import Promotion from './promotion/Promotion';
+import VideoModal from './video-modal/VideoModal';
+import Portfolio from './portfolio/Portfolio';
+import Products from './products-carousel/ProductsCarousel';
+import Team from './team/Team';
 
-    return(
-        <>
-        <Carousel />
-        <About />
-        <Promotion/>
-        <VideoModal/>
-        <Portfolio />
-        <Products />
-        <Team />
-        </>
-    )
+export default function Home() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [videoSrc, setVideoSrc] = useState('');
+
+  const handleOpenModal = (url) => {
+    setVideoSrc(url);
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Carousel />
+      <About />
+      <Promotion onOpenModal={handleOpenModal} />
+      <VideoModal
+        isVisible={isModalVisible}
+        onClose={handleCloseModal}
+        videoSrc={videoSrc}
+      />
+      <Portfolio />
+      <Products />
+      <Team />
+    </>
+  );
 }
