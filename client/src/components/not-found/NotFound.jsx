c24a8ef089a6cdaa9bf7623e7017eecad90bae68
StilyanaN <134604297+StilyanaN@users.noreply.github.com>
2024-08-07 18:31:19 +0300

    Add scroll handler

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -1,17 +1,17 @@
-import styles from "./NotFound.module.css"
 import { Link } from 'react-router-dom';
-
+import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-    return (
-        
-      <div className={styles['not-found-container']}>
-            <img src="img/not-found-page.jpg" alt="404 error" className={styles['not-found-img']}/>
-            
-            <Link to='/' className="btn btn-primary py-md-3 px-md-5 mt-2">Go Back Home</Link>
-        </div>
-      
-    );
+    const handleScrollToTop = () => {
+        window.scrollTo(0, 0);
+      };
+
+  return (
+    <div className={styles['not-found-container']}>
+      <img src="img/not-found-page.jpg" alt="404 error" className={styles['not-found-img']} />
+      <Link to='/' className="btn btn-primary py-md-3 px-md-5 mt-2" onClick={handleScrollToTop}>Go Back Home</Link>
+    </div>
+  );
 };
 
 export default NotFound;
